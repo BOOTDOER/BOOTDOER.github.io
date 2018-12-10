954d0c0f149ce1324b1917c4fa44e59d6c25c8c3
Tony <2742974101@qq.com>
2018-12-10 21:34:44 +0800

    重新整合

---
+++ js/index.js
@@ -0,0 +1,26 @@
+new Vue({
+        el: '#app',
+        data: {
+          nav: [
+            {
+              name: "关于我",
+              url: "#"
+            },
+            {
+              name: "分享",
+              url: "#"
+            },
+            {
+              name: "兴趣",
+              url: "#"
+            },
+            {
+              name: "留言板",
+              url: "#"
+            },
+          ]
+        },
+        methods: {
+          
+        }
+      }) 
